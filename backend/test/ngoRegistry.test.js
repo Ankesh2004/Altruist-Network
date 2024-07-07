c1e803449236f9e9109f17bbe60baf6421e267f7
Ankesh2004 <gankesh808@gmail.com>
2024-07-07 14:42:49 +0530

    fix: replaced depriciated functions

--- backend/test/ngoRegistry.test.js
+++ backend/test/ngoRegistry.test.js
@@ -5,23 +5,23 @@ describe("ngoRegistry", function () {
 
   beforeEach(async function () {
     [owner, addr1] = await ethers.getSigners();
-    DonorRegistry = await ethers.getContractFactory("donorRegistry");
+    DonorRegistry = await ethers.getContractFactory("DonorRegistry");
     donorRegistry = await DonorRegistry.deploy();
-    await donorRegistry.deployed();
+    await donorRegistry.waitForDeployment();
 
-    NGORegistry = await ethers.getContractFactory("ngoRegistry");
-    ngoRegistry = await NGORegistry.deploy(donorRegistry.address);
-    await ngoRegistry.deployed();
+    NGORegistry = await ethers.getContractFactory("NGORegistry");
+    ngoRegistry = await NGORegistry.deploy(donorRegistry.target);
+    await ngoRegistry.waitForDeployment();
   });
 
   it("Should register an NGO", async function () {
     await ngoRegistry.connect(addr1).registerNGO("Helpful NGO", "NGO Description", "0x");
-    expect(await ngoRegistry.isRegisteredNGO(addr1.address)).to.equal(true);
+    expect(await ngoRegistry.isRegisteredNGO(addr1.target)).to.equal(true);
   });
 
   it("Should emit NGORegistered event", async function () {
     await expect(ngoRegistry.connect(addr1).registerNGO("Helpful NGO", "NGO Description", "0x"))
       .to.emit(ngoRegistry, 'NGORegistered')
-      .withArgs("Helpful NGO", "NGO Description", "0x", addr1.address);
+      .withArgs("Helpful NGO", "NGO Description", "0x", addr1.target);
   });
 });
