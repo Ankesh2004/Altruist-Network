ec8dc49b51603706efed1d32366c4528cf313e2b
Ankesh2004 <gankesh808@gmail.com>
2024-07-09 01:44:14 +0530

    fix: Hero section UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,22 +4,22 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       {/* Hero Section  */}
-      <section className="flex flex-wrap items-center w-full rounded-lg border justify-around gap-4 p-4">
+      <section className="flex flex-wrap items-center w-full rounded-lg border justify-around gap-4 px-4 py-12">
         {/* Texts  */}
         <div className="flex flex-col gap-16 lg:max-w-[60%]">
           {/* hero text  */}
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-bold text-left">
+            <h1 className="sm:text-4xl text-3xl font-bold lg:text-left text-center">
               Empowering Communities Through Decentralized Giving
             </h1>
-            <p className="text-xl text-left">
+            <p className="sm:text-xl text-lg lg:text-left text-center">
               Our decentralized platform connects donors directly with
               grassroots organizations, ensuring your contributions have the
               greatest impact.
             </p>
           </div>
           {/* hero buttons  */}
-          <div className="flex flex-wrap gap-4">
+          <div className="flex flex-wrap gap-4 lg:justify-start justify-center">
           <Button variant="filled" color="#065471" size="lg" radius="lg">Donate</Button>
           <Button variant="outline" color="#065471" size="lg" radius="lg">Register as NGO</Button>
           </div>
