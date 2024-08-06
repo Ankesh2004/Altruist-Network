8414930557b33f9391361ef38526d250733c0e54
Ankesh2004 <gankesh808@gmail.com>
2024-08-06 10:24:02 +0530

    feat(onboarding-ngo): Dropzone for files upload

--- frontend/src/app/onboarding/ngo/page.tsx
+++ frontend/src/app/onboarding/ngo/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React, { useState } from "react";
-import Dice from "../../../../public/icons/Dice";
+import Dropzone from "./Dropzone";
 import { Input, Group, HoverCard, Button, Text } from "@mantine/core";
 import { generateUsername } from "unique-username-generator";
 
 const page = () => {
   const [username, setUsername] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    documents: [],
+  });
   const generateRandomUsername = () => {
     const randomUsername = generateUsername("", 4);
     setUsername(randomUsername);
@@ -13,42 +18,39 @@ const page = () => {
   return (
     <div className="flex flex-col gap-8 items-center p-8">
       <div className="flex flex-col gap-2 text-center">
-        <h1 className="text-secondaryDarkColor text-2xl  font-semibold">
+        <h1 className="text-secondaryDarkColor text-2xl font-semibold ">
           Fill the NGO details
         </h1>
-        <p className="text-lightGray text-xs">
-          <span className="text-red-500"></span> Do not use your real name for
-          privacy concerns
-        </p>
       </div>
-      <div className="flex flex-col gap-2 items-center px-4 py-3 rounded-lg shadow bg-green-400">
-        <div>
-          <h3 className="text-lg font-semibold">Name:</h3>
+      <div className="flex flex-col gap-2 items-center px-4 py-3 rounded-lg shadow border ">
+        <div className="md:w-[32rem]">
+          <h3 className="text-md ">Name:</h3>
           <Input
             size="md"
             radius="md"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            placeholder="NGO Name"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
           />
         </div>
-        <div>
-          <h3 className="text-lg font-semibold">Description:</h3>
+        <div className="md:w-[32rem]">
+          <h3 className="text-md ">Description:</h3>
           <Input
             size="md"
             radius="md"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Dewcribe your NGO"
+            value={formData.description}
+            onChange={(e) => setFormData({ ...formData, description: e.target.value })}
           />
         </div>
-        <div>
-          <h3 className="text-lg font-semibold">NGO Authenticity Documents</h3>
+        <div className="md:w-[32rem]">
+          <h3 className="text-md ">NGO Authenticity Documents</h3>
           {/* Dropzone  */}
+          <Dropzone />
         </div>
       </div>
 
-      {username !== "" && (
+  
         <Button
           variant="filled"
           color="#065471"
@@ -56,9 +58,9 @@ const page = () => {
           radius="md"
           className="transition transform"
         >
-          Continue as {username}
+          Regsister NGO
         </Button>
-      )}
+      
     </div>
   );
 };
